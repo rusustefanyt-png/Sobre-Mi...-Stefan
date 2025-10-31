8ea0abaf3203d07fbfb0b2f360440b0e7b1d6fb0
rusustefanyt-png <rusustefanyt@gmail.com>
2025-10-31 12:17:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function calcularEdad() {
+  const nacimiento = new Date('2008-12-05'); // Cumpleaños 5 diciembre 2009
+  const hoy = new Date();
+  let edad = hoy.getFullYear() - nacimiento.getFullYear();
+
+  const cumpleEsteAño = new Date(hoy.getFullYear(), nacimiento.getMonth(), nacimiento.getDate());
+  if (hoy < cumpleEsteAño) edad--;
+
+  document.getElementById('edad').textContent = edad;
+}
+
+calcularEdad();
